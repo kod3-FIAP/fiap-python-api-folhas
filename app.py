165532a619937ffa8ecae424224dcece1ffd4092
Gabriel Bianchi <gdbianchii@gmail.com>
2025-10-05 12:59:36 -0300

    CORS

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from datetime import datetime
 
 import pandas as pd
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 DATASET_PATH = "./folhas_dataset.csv"
 
